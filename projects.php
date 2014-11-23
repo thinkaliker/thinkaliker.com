039cd2af59c6f36a3affb94e30aa2ae1bc5b6d65
Adam Chao <adam@thinkaliker.com>
2014-11-22 21:20:59 -0800

    added github link

--- projects.php
+++ projects.php
@@ -1,15 +1,17 @@
 <div class="inner cover" id="projects">
     <h2 class="cover-heading">Projects<small> (Stuff I'm working on)</small></h2>
+    <h3>Github</h3>
+        <li class="link"><p class="lead"><a href="http://github.com/thinkaliker" target="_blank">Github</a> - this is what you wanted, right? Repositories and more.</p></li>
     <h3>Websites</h3>
-    <li class="link"><p class="lead"><a href="http://rhocode.com" target="_blank">rhoCode</a> - Consultant/Co-founder</p></li>
-    <li class="link"><p class="lead"><a href="http://res-ex.net" target="_blank">Res-Ex</a> - UX/Frontend</p></li>
-    <li class="link"><p class="lead"><a href="http://auroraserver.tk" target="_blank">AuroraServer</a> - Design/Maintainence</p></li>
-    <li class="link"><p class="lead"><a href="http://sandiegotzushao.com" target="_blank">San Diego Tzu Shao</a> - Desgin</p></li>
-    <li class="link"><p class="lead"><a href="index.php">thinkaliker</a> - Design/Maintainance</p></li>
+        <li class="link"><p class="lead"><a href="http://rhocode.com" target="_blank">rhoCode</a> - Consultant/Co-founder</p></li>
+        <li class="link"><p class="lead"><a href="http://res-ex.net" target="_blank">Res-Ex</a> - UX/Frontend</p></li>
+        <li class="link"><p class="lead"><a href="http://auroraserver.tk" target="_blank">AuroraServer</a> - Design/Maintainence</p></li>
+        <li class="link"><p class="lead"><a href="http://sandiegotzushao.com" target="_blank">San Diego Tzu Shao</a> - Desgin</p></li>
+        <li class="link"><p class="lead"><a href="index.php">thinkaliker</a> - Design/Maintainance</p></li>
     <h3>Android</h3>
-    <li class="link"><p class="lead"><a href="http://github.com/thinkaliker/slash" target="_blank">slash [WIP]</a></p></link>
+        <li class="link"><p class="lead"><a href="http://github.com/thinkaliker/slash" target="_blank">slash [WIP]</a></p></link>
     <h3>Moderation</h3>
-    <li class="link"><p class="lead"><a href="http://www.reddit.com/r/creepscomic">/r/creepscomic</a></p></li>
-    <li class="link"><p class="lead"><a href="http://www.reddit.com/r/wordhero">/r/wordhero</a></p></li>
-    <li class="link"><p class="lead"><a href="http://www.reddit.com/r/ailise">/r/ailise</a></p>
+        <li class="link"><p class="lead"><a href="http://www.reddit.com/r/creepscomic">/r/creepscomic</a></p></li>
+        <li class="link"><p class="lead"><a href="http://www.reddit.com/r/wordhero">/r/wordhero</a></p></li>
+        <li class="link"><p class="lead"><a href="http://www.reddit.com/r/ailise">/r/ailise</a></p>
 </div>
