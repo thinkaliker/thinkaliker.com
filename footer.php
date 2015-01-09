ab17f167ad3ad320202aaa8a025d60a57483d805
Adam Chao <adam@thinkaliker.com>
2015-01-09 13:33:32 -0800

    happy new year

--- footer.php
+++ footer.php
@@ -1,1 +1,1 @@
-<p>&copy; thinkaliker (Adam Chao) 2014 - Created with Bootstrap - Dogecoin: DDoTjX1Zgp7CnSFhXjYeWeTdujbTgF6Wix</p>
+<p>&copy; thinkaliker (Adam Chao) 2015 - Created with Bootstrap - Dogecoin: DDoTjX1Zgp7CnSFhXjYeWeTdujbTgF6Wix</p>
