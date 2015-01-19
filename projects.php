22cb3bc85d0af5f6b2c49897b4219d1605c77bdb
Adam Chao <adam@thinkaliker.com>
2015-01-19 00:17:30 -0800

    formatting fix

--- projects.php
+++ projects.php
@@ -2,18 +2,17 @@
 <div class="inner cover" id="projects">
     <view-title>Projects</view-title>
     <h2 class="cover-heading">Projects<small> (Stuff I'm working on)</small></h2>
-    <h3>Github</h3>
-        <li class="link"><p class="lead"><a href="http://github.com/thinkaliker" target="_blank">Github</a> - this is what you wanted, right? Repositories and more.</p></li>
+        <li class="link"><a href="http://github.com/thinkaliker" target="_blank">Github</a> - this is what you wanted, right? Repositories and more.</li>
     <h3>Websites</h3>
-        <li class="link"><p class="lead"><a href="http://rhocode.com" target="_blank">rhoCode</a> - Consultant/Co-founder</p></li>
-        <li class="link"><p class="lead"><a href="http://res-ex.net" target="_blank">Res-Ex</a> - UX/Frontend</p></li>
-        <li class="link"><p class="lead"><a href="http://auroraserver.tk" target="_blank">AuroraServer</a> - Design/Maintainence</p></li>
-        <li class="link"><p class="lead"><a href="http://sandiegotzushao.com" target="_blank">San Diego Tzu Shao</a> - Desgin</p></li>
-        <li class="link"><p class="lead"><a href="index.php">thinkaliker</a> - Design/Maintainance</p></li>
+        <li class="link"><a href="http://rhocode.com" target="_blank">rhoCode</a> - Consultant/Co-founder</li>
+        <li class="link"><a href="http://res-ex.net" target="_blank">Res-Ex</a> - UX/Frontend</li>
+        <li class="link"><a href="http://auroraserver.tk" target="_blank">AuroraServer</a> - Design/Maintainence</li>
+        <li class="link"><a href="http://sandiegotzushao.com" target="_blank">San Diego Tzu Shao</a> - Desgin</li>
+        <li class="link"><a href="index.php">thinkaliker</a> - Design/Maintainance</p></li>
     <h3>Android</h3>
-        <li class="link"><p class="lead"><a href="http://github.com/thinkaliker/slash" target="_blank">slash [WIP]</a></p></link>
+        <li class="link"><a href="http://github.com/thinkaliker/slash" target="_blank">slash [WIP]</a></li>
     <h3>Moderation</h3>
-        <li class="link"><p class="lead"><a href="http://www.reddit.com/r/creepscomic">/r/creepscomic</a></p></li>
-        <li class="link"><p class="lead"><a href="http://www.reddit.com/r/wordhero">/r/wordhero</a></p></li>
-        <li class="link"><p class="lead"><a href="http://www.reddit.com/r/ailise">/r/ailise</a></p>
+        <li class="link"><a href="http://www.reddit.com/r/creepscomic">/r/creepscomic</a></li>
+        <li class="link"><a href="http://www.reddit.com/r/wordhero">/r/wordhero</a></li>
+        <li class="link"><a href="http://www.reddit.com/r/ailise">/r/ailise</a></li>
 </div>
