01e8fb99399b04e1b38b8d501523bd77ad88323e
Adam Chao <adam@thinkaliker.com>
2015-01-19 00:17:06 -0800

    link overhaul

--- links.php
+++ links.php
@@ -3,60 +3,26 @@
     <view-title>Links</view-title>
 
     <h2 class="cover-heading text-center">Links <small>(where to find me)</small></h2>
-
-    <dl class="dl-horizontal">
-        <dt><p class="link"><a href="http://www.twitter.com/thinkaliker" target="_blank">Twitter</a></p></dt>
-        <dd class="pull-left"><p class=""><iframe allowtransparency="true" frameborder="0" scrolling="no" src="//platform.twitter.com/widgets/follow_button.html?screen_name=thinkaliker" style="width:300px; height:20px;"></iframe></p></dd>
-    
-        <dt><p class="link"><a href="http://www.youtube.com/thinkaliker" target="_blank">YouTube</a></p></dt>
-        <dd class="pull-left"><p class=""><div class="g-ytsubscribe" data-channel="thinkaliker" data-layout="default"></div></p></dd>
-    
-        <dt><p class="link"><a href="http://www.instagram.com/thinkaliker" target="_blank">Instagram</a></p></dt>
-        <dd class="pull-left"><a href="http://instagram.com/thinkaliker?ref=badge" class="ig-b- ig-b-v-24"><img src="//badges.instagram.com/static/images/ig-badge-view-24.png" alt="Instagram" /></a></dd>
-    
-        <dt height="63"><p class="link"><a href="http://www.facebook.com/thinkaliker" target="_blank">Facebook</a></p></dt>
-        <dd class="pull-left"><iframe src="//www.facebook.com/plugins/follow.php?href=https%3A%2F%2Fwww.facebook.com%2Fthinkaliker&amp;width&amp;height=35&amp;colorscheme=dark&amp;layout=button&amp;show_faces=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:35px; padding-top:5px;" allowTransparency="true"></iframe></dd>
-    
-        <dt><p class="link"><a href="http://plus.google.com/+AdamChao" target="_blank">Google+</a></p></dt>
-        <dd class="pull-left"><a href="//plus.google.com/+AdamChao" rel="publisher" target="_top" style="text-decoration:none;display:inline-block;color:#fff;text-align:center; font:13px/16px arial,sans-serif;white-space:nowrap;">
-    <span style="display:inline-block;font-weight:bold;vertical-align:top;margin-right:5px; margin-top:8px;">Adam</span><span style="display:inline-block;vertical-align:top;margin-right:15px; margin-top:8px;">on</span>
-    <img src="//ssl.gstatic.com/images/icons/gplus-32.png" alt="Google+" style="border:0;width:32px;height:32px;"/></a></dd>
-
-        <dt><p class="link"><a href="http://adamchao.me" target="_blank">About.me</a></p></dt>
-        <dd class="pull-left">About.me profile!</dd>
-    
-        <dt><p class="link"><a href="http://vine.co/thinkaliker" target="_blank">Vine</a></p></dt>
-        <dd class="pull-left">Vine!</dd>
-    
-        <dt><p class="link"><a href="http://plus.google.com/+thinkalikertk" target="_blank">Other Google+</a></p></dt>
-        <dd class="pull-left"><a href="//plus.google.com/+thinkalikertk" rel="publisher" target="_top" style="text-decoration:none;display:inline-block;color:#fff;text-align:center; font:13px/16px arial,sans-serif;white-space:nowrap;">
-    <span style="display:inline-block;font-weight:bold;vertical-align:top;margin-right:5px; margin-top:8px;">thinkaliker</span><span style="display:inline-block;vertical-align:top;margin-right:15px; margin-top:8px;">on</span>
-    <img src="//ssl.gstatic.com/images/icons/gplus-32.png" alt="Google+" style="border:0;width:32px;height:32px;"/></a></dd>
-    
-    
-        <dt><p class="link"><a href="http://www.linkedin.com/in/adamchao1" target="_blank">LinkedIn</a></p></dt>
-        <dd class="pull-left"><a href="http://www.linkedin.com/in/adamchao1"><img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_liprofile_blue_80x15.png" width="80" height="15" border="0" alt="View Adam Chao's profile on LinkedIn"></a></dd>
-    
-        <dt><p class="link"><a href="http://steamcommunity.com/id/thinkaliker/">Steam</a></p></dt>
-        <dd class="pull-left">Video Games!</dd>
-
-        <dt><p class="link"><a href="http://twitch.tv/thinkaliker/">Twitch</a></p></dt>
-        <dd class="pull-left">Twitch streaming!</dd>
-
-        <dt><p class="link"><a href="http://player.me/thinkaliker">Player.me</a></p></dt>
-        <dd class="pull-left">Player.me profile!</dd>
-    
-        <dt><p class="link"><a href="http://www.soundcloud.com/thinkaliker" target="_blank">Soundcloud</a></p></dt>
-        <dd class="pull-left">Music!</dd>
-    
-        <dt><p class="link"><a href="http://thinkaliker.deviantart.com/" target="_blank">DeviantArt</a></p></dt>
-        <dd class="pull-left">Artwork!</dd>
-    
-        <dt><p class="link"><a href="http://github.com/thinkaliker" target="_blank">GitHub</a></p></dt>
-        <dd class="pull-left"><iframe src="http://ghbtns.com/github-btn.html?user=thinkaliker&type=follow"
-    allowtransparency="true" frameborder="0" scrolling="0" width="135" height="20"></iframe></dd>
-    
-        <dt><p class="link"><a href="http://thinkaliker.bandcamp.com" target="_blank">Bandcamp</a></p></dt>
-        <dd class="pull-left">Currently empty!</dd>
-    </dl>
+    <br />
+    <p class="text-center">
+    <a class="btn btn-default btn-lg btnclear" href="http://www.twitter.com/thinkaliker" target="_blank"><i class="fa fa-twitter"></i> Twitter</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://www.youtube.com/thinkaliker" target="_blank"><i class="fa fa-youtube-play"></i> YouTube</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://www.instagram.com/thinkaliker" target="_blank"><i class="fa fa-instagram"></i> Instagram</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://www.facebook.com/thinkaliker" target="_blank"><i class="fa fa-facebook"></i> Facebook</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://plus.google.com/+AdamChao" target="_blank"><i class="fa fa-google-plus"></i> Google+</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://github.com/thinkaliker" target="_blank"><i class="fa fa-github-alt"></i> GitHub</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://bitbucket.org/thinkaliker" target="_blank"><i class="fa fa-bitbucket"></i> BitBucket</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://adamchao.me" target="_blank"><i class="fa fa-briefcase"></i> About.me</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://vine.co/thinkaliker" target="_blank"><i class="fa fa-vine"></i> Vine</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://plus.google.com/+thinkalikertk" target="_blank"><i class="fa fa-google-plus"></i> Other Google+</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://www.linkedin.com/in/adamchao1" target="_blank"><i class="fa fa-linkedin"></i> LinkedIn</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://steamcommunity.com/id/thinkaliker/"><i class="fa fa-steam"></i> Steam</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://twitch.tv/thinkaliker/"><i class="fa fa-twitch"></i> Twitch</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://player.me/thinkaliker"><i class="fa fa-globe"></i> Player.me</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://www.soundcloud.com/thinkaliker" target="_blank"><i class="fa fa-soundcloud"></i> SoundCloud</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://thinkaliker.deviantart.com/" target="_blank"><i class="fa fa-deviantart"></i> DeviantArt</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://reddit.com/user/thinkaliker" target="_blank"><i class="fa fa-reddit"></i> reddit</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://thinkaliker.tumblr.com" target="_blank"><i class="fa fa-tumblr"></i> Tumblr</a>
+    <a class="btn btn-default btn-lg btnclear" href="http://thinkaliker.bandcamp.com" target="_blank"><i class="fa fa-headphones"></i> Bandcamp</a>
+    </p>
 </div>
