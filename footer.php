57ec6c645adc64e9c27beea3c42d94aff08a4d9e
Adam Chao <adam@thinkaliker.com>
2015-02-19 16:17:32 -0800

    moved dogecoin address

--- footer.php
+++ footer.php
@@ -1,1 +1,1 @@
-<p>&copy; thinkaliker (Adam Chao) 2015 - Created with Bootstrap/AngularJS/PHP - <small>Dogecoin: DDoTjX1Zgp7CnSFhXjYeWeTdujbTgF6Wix</small></p>
+<p>&copy; thinkaliker (Adam Chao) 2015 - Created with Bootstrap/AngularJS/PHP</p>
