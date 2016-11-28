2b2514e2438c3b4a80380a7b58d98112bb957954
Adam Chao <adam@thinkaliker.com>
2016-11-27 20:55:59 -0800

    css/js cleanup

--- js/extras.js
+++ js/extras.js
@@ -1,17 +1,17 @@
-WebFontConfig = {
-  google: {
-    families: ['Roboto::latin']
-  }
-};
-(function() {
-  var wf = document.createElement('script');
-  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-  wf.type = 'text/javascript';
-  wf.async = 'true';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(wf, s);
-})();
+//WebFontConfig = {
+//  google: {
+//    families: ['Roboto::latin']
+//  }
+//};
+//(function() {
+//  var wf = document.createElement('script');
+//  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+//    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+//  wf.type = 'text/javascript';
+//  wf.async = 'true';
+//  var s = document.getElementsByTagName('script')[0];
+//  s.parentNode.insertBefore(wf, s);
+//})();
 
 //(function(i, s, o, g, r, a, m) {
 //  i['GoogleAnalyticsObject'] = r;
