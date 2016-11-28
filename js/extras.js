076e709ce89489ee8dca64cc7635d2883f7b8c8e
Adam Chao <adam@thinkaliker.com>
2016-11-27 21:08:39 -0800

    more cleanup, undo mdl js move

--- js/extras.js
+++ js/extras.js
@@ -1,40 +1,31 @@
-//WebFontConfig = {
-//  google: {
-//    families: ['Roboto::latin']
-//  }
-//};
-//(function() {
-//  var wf = document.createElement('script');
-//  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-//    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-//  wf.type = 'text/javascript';
-//  wf.async = 'true';
-//  var s = document.getElementsByTagName('script')[0];
-//  s.parentNode.insertBefore(wf, s);
-//})();
+WebFontConfig = {
+  google: {
+    families: ['Roboto::latin']
+  }
+};
+(function() {
+  var wf = document.createElement('script');
+  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})();
 
-//(function(i, s, o, g, r, a, m) {
-//  i['GoogleAnalyticsObject'] = r;
-//  i[r] = i[r] || function() {
-//    (i[r].q = i[r].q || []).push(arguments)
-//  }, i[r].l = 1 * new Date();
-//  a = s.createElement(o),
-//  m = s.getElementsByTagName(o)[0];
-//  a.async = 1;
-//  a.src = g;
-//  m.parentNode.insertBefore(a, m)
-//})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-//
-//ga('create', 'UA-37558861-4', 'auto');
-//ga('send', 'pageview');
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+  m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-$(document).ready(function() {
-  var links = Array.prototype.slice.call(document.querySelectorAll('.mdl-navigation__link'));
+ga('create', 'UA-37558861-4', 'auto');
+ga('send', 'pageview');
 
-  links.forEach(function(link) {
-    link.addEventListener('click', function() {
-      document.getElementById("drawer").className = "mdl-layout__drawer";
-      $('.mdl-layout__drawer, .mdl-layout__obfuscator').removeClass('is-visible');
-    });
-  });
-});
+
